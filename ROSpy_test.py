e9cb616b1fc17e6239347f2c7d89481fe8c7a329
ReconCycle <webmaster@reconcycle.eu>
2023-03-16 14:29:57 +0100

    Updated publisher format

--- ROSpy_test.py
+++ ROSpy_test.py
@@ -7,7 +7,8 @@ from sensor_msgs.msg import Image
 from std_srvs.srv import Trigger
 from cv_bridge import CvBridge   
 
-from PIL import Image, ImageFilter
+from PIL import Image as PILimage
+from PIL import ImageFilter
         
 
 class TestCamROSpy():
@@ -19,18 +20,23 @@ class TestCamROSpy():
         self.watcher = rospy.Subscriber("/realsense/color/image_raw", Image, self.transimg)
 
         # Definicija publisherja
-        self.poser = rospy.Publisher("/realsense/color/image_pose", Image, queue_size = 1)
+        self.poser = rospy.Publisher("/alphapose", Image, queue_size = 1)
 
         rospy.spin()
 
     def transimg(self, input: Image) -> Image:
+        print(input)
         image = CvBridge().imgmsg_to_cv2(input, desired_encoding='rgb8')
-        pil_image = Image.fromarray(image)
+        pil_image = PILimage.fromarray(image)
         pil_image.save("slika.jpg")
         receivedImg = pil_image
         editedImg = receivedImg.filter(ImageFilter.BLUR)
+
+        ros_edited_img = CvBridge().cv2_to_imgmsg(editedImg, encoding = 'rgb8')
+
+        input.data = ros_edited_img
         
-        self.poser.publish(editedImg)
+        self.poser.publish(input)
 
     
 
